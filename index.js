05263b1f8a7f312c9a3e2c75dc5eca86e2c5d278
Rushikesh0527 <rushidhatinge@gmail.com>
2026-01-20 13:12:00 +0530

    Fixed all errors

--- index.js
+++ index.js
@@ -6,47 +6,50 @@ import bodyParser from "body-parser"
 
 const app = express();
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(morgan("tiny"));
-const port = 3000 ; 
- let titleArray =[];
-    let writingArray =[];
+const port = 3000;
+let blogs = [];
+let idCounter = 0;
 
 
-app.get("/", (req,res)=>{
-    res.render("index.ejs")
+app.get("/", (req, res) => {
+    res.render("index.ejs", { blogs })
 })
 
-app.get("/submit", (req,res)=>{
-    res.render("newBlog.ejs",{titleArray})
+app.get("/blog/:id", (req, res) => { const blog = blogs.find(b => b.id == req.params.id);
+
+
+
+     if (!blog) {
+    return res.send("Blog not found");
+  }
+     res.render("blog.ejs", { blog }); });
+
+
+
+app.get("/newBlog", (req, res) => {
+    res.render("newBlog.ejs")
 })
 
-app.get("/blog", (req,res)=>{
-    res.render("Blog.ejs",{titleArray})
+
+app.post("/submit", (req, res) => {
+
+    const blog = {
+        id: idCounter++,
+        title: req.body.title,
+        content: req.body.content
+
+    }
+    blogs.push(blog);
+    console.log(blogs)
+    res.redirect("/")
+
+
 })
 
-   
-    
-    // writingArray.push(writing);
-
-app.post("/submit",(req,res)=>{
-//  let writing = req.body.writing;
-
-    let Title = req.body.title;
-   titleArray.push(Title);
-  
-
-
-  
-    
-    res.render("index.ejs",{titleArray})
-    
-    console.log(titleArray)
-    
-})
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Your server is running")
 })
 
@@ -71,6 +74,10 @@ app.set("view engine", "ejs");
                           <h1>  write something </h1>
                         <% } %>
 
+                        I am using wrong approach Instead of using object I using 
+                        two arrays separate arrays or content title and I should add ID to an object also 
+
+                        I forgot let or var
 
 */
 
